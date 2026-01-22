a421f48480320a50de0538a4b66a8dfac7509e5b
Mallory Joy <malloryjoy13@gmail.com>
2026-01-22 18:34:58 -0500

    Added comments for process

--- pythonChallenges/January22.py
+++ pythonChallenges/January22.py
@@ -1,13 +1,43 @@
-import math
+##Given an array of exam scores (numbers), return the average score in form of a letter grade according to 
+##the following chart:
+
+##      Average Score	Letter Grade
+##      97-100	        "A+"
+##      93-96	        "A"
+##      90-92	        "A−"
+##      87-89	        "B+"
+##      83-86	        "B"
+##      80-82	        "B-"
+##      77-79	        "C+"
+##      73–76	        "C"
+##      70-72	        "C-"
+##      67-69	        "D+"
+##      63-66	        "D"
+##      60–62	        "D-"
+##      below 60       	"F"
+
+## Calculate the average by adding all scores in the array and dividing by the total number of scores.
+
+
+##      1. getAverageGrade([92, 91, 90, 94, 89, 93]) should return "A-".
+##      2. getAverageGrade([84, 89, 85, 100, 91, 88, 79]) should return "B+".
+##      3. getAverageGrade([63, 69, 65, 66, 71, 64, 65]) should return "D".
+##      4. getAverageGrade([97, 98, 99, 100, 96, 97, 98, 99, 100]) should return "A+".
+##      5. getAverageGrade([75, 100, 88, 79, 80, 78, 64, 60]) should return "C+".
+##      6. getAverageGrade([45, 48, 50, 52, 100, 54, 56, 58, 59]) should return "F".
+
+
+
+import math ##import math module 
 
 
 def get_average_grade(scores):
-    meanScore = math.floor(sum(scores) / len(scores));
-    print (meanScore)
-    letterGrade ="";
-    if meanScore >= 97 and meanScore <= 100:
+    meanScore = math.floor(sum(scores) / len(scores)); ## declare value for average of scores, rounded down to nearest whole number
+    print (meanScore) ## check that the value is correct
+    letterGrade =""; ## declare letterGrade as an empty string value
+    if meanScore >= 97 and meanScore <= 100: ## create if/elif statement for all values/corresponding letter grades
         letterGrade="A+";
-        print("A+")
+        print("A+") ## check to ensure selected letterGrade is correct
     elif meanScore >= 93 and meanScore <= 96:
         letterGrade="A";
         print("A");
@@ -44,7 +74,7 @@ def get_average_grade(scores):
     elif meanScore <= 59:
         letterGrade="F";
         print("F");         
-    return letterGrade;
+    return letterGrade; ## return letterGrade
 
 
 get_average_grade([92, 91, 90, 94, 89, 93])
