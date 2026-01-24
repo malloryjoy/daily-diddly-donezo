1e8ac0523cc5bee55a0b1543156a328fd821f517
Mallory Joy <malloryjoy13@gmail.com>
2026-01-23 19:28:57 -0500

    Removed excess code and added thought-process comments

--- javascriptChallenges/January23.js
+++ javascriptChallenges/January23.js
@@ -8,77 +8,47 @@ Hexadecimal characters are numbers 0 through 9 and letters a through f (case-ins
 
 function isValidHex(str) {
   
-    const array = str.split("");
+    const array = str.split(""); //place characters in provided test string in an array
     //console.log(array);
-  
-    //console.log(array[0])
-    //console.log(array.length)
-  
-    const regexp = /^[0-9a-fA-F]+$/;
-  
-    function is_hexadecimal(array)
-  {
-   regexp = /^[0-9a-fA-F]+$/;
-    
-          if (regexp.test(array))
-            {
-              console.log("TRUE");
-              return true;
-            }
-          else
-            {
-              console.log("FALSE");
-              return false;
-            }
-  }
-    
   
    
   
-    let hexCheck = "";
-  const checkhex = (array) => {
+    const regexp = /^[0-9a-fA-F]+$/; //declares regex; character checker to make sure characters are alphanumeric
+  
+  
+   
+  
+    let hexCheck = ""; //create empty string 
+  const checkhex = (array) => { //declares variable to check each character of array 
     array.forEach((item) => {
-      if (regexp.test(item)) {
-        hexCheck += 'y';
+      if (regexp.test(item)) { //for each item in the array, check if the value is alphanumeric 
+        hexCheck += 'y'; //if  it is alphanumeric, add a y value to the empty string 
       } else {
-        hexCheck+= 'n';
+        hexCheck+= 'n'; //if it is NOT alphanumeric, add a n value to the empty string 
       }
     });
-    return hexCheck;
+    return hexCheck; //return converted array/search completed string
   };
   
-  checkhex(array)
-  //console.log(hexCheck)
+  checkhex(array) //perform this test on the array created from the string
   
-  let originalString = hexCheck;
-  //console.log(hexCheck);
-  let charToRemove = /n/g; // Regular expression with global flag
-  let newString = originalString.replace(charToRemove, "")
-  //console.log(newString);
-  
+    console.log(hexCheck);
+    let charToRemove = /n/g; // Regular expression with global flag
+    let newString = hexCheck.replace(charToRemove, "") // create new string after replacing any "ns in the developed string"
+    console.log(newString);
+
   
   
-  if ((newString === "yyyyyy" || newString ==="yyy") && (array[0] == "#") && (!str.includes(' '))){
-    console.log(true)
-    return true; 
+  
+  if ((newString === "yyyyyy" || newString ==="yyy") && (array[0] == "#") && (!str.includes(' '))){ //sets if statement for if the newstring is either 3 y's or 6y's, as well as if the first value in the array is a hashtag, and whether or not the string has a space in it --making it invalid.
+    console.log(true) 
+    return true; //returns true if the test string could be a hex code
   } else {
     console.log(false)
-    return false;
+    return false; //returns false if the test string is not a hex code 
   }
   
-    /*if (array.length == 6 && array[0] == "#" && hexCheck ){
-      console.log("Yay!")
-    } else {
-      console.log("No!")
-    } */
-    
-    /*let coleur="";
-      if () {
-        coleur = true
-      } else{
-        coleur = false
-      }
-    return coleur;*/
+
   }
   
   isValidHex("#123") 
