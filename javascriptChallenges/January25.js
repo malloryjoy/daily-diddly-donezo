a229bd38e849209e58510c1e026a09d0c309ddf2
Mallory Joy <malloryjoy13@gmail.com>
2026-01-25 11:15:28 -0500

    Added comments to explain thought process in solution

--- javascriptChallenges/January25.js
+++ javascriptChallenges/January25.js
@@ -5,16 +5,16 @@ The input string is in the format "WxH". For example, "800x600".
 The scale is a number to multiply the width and height by.
 Return the scaled dimensions in the same "WxH" format. */
 
-function scaleImage(size, scale) {
-    let iArray = size.split("x");
-    let width = iArray[0] * scale; 
-    let length = iArray[1] * scale;
-    //console.log(iArray);
-    //console.log(width);
-    //console.log(length);
-    let sizeConvert = width + "x" + length;
-    console.log(sizeConvert);
-    return sizeConvert;
+function scaleImage(size, scale) { //establish function that takes size and scale as parameters
+    let iArray = size.split("x"); //creates an array using the values provided in the size  
+    let width = iArray[0] * scale; //creates a variable using the first item in the array (width)
+    let length = iArray[1] * scale; //creates a variable using the second item in the array (length)
+    //console.log(iArray); to test array line works 
+    //console.log(width); to test that the width value works 
+    //console.log(length); to test that the length value works 
+    let sizeConvert = width + "x" + length; //creates variable that establishes the answer as the width "multiplied" by the length
+    console.log(sizeConvert); //console.log that checks function works 
+    return sizeConvert; //return correct answer 
   }
   
   scaleImage("800x600", 2) //should return "1600x1200".
