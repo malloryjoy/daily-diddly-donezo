72aa988cbda1c770a08356f7fa430a6dca60d4b9
Mallory Joy <malloryjoy13@gmail.com>
2026-01-26 19:10:50 -0500

    Provided comments with answers

--- javascriptChallenges/January26.js
+++ javascriptChallenges/January26.js
@@ -6,17 +6,17 @@ If the number is divisible by both 3 and 5, return "FizzBuzz".
 Otherwise, return the given number as a string.*/ 
 
 function fizzBuzzMini(n) {
-    let FizzBo; 
-    if (n%5==0 && n%3==0){
+    let FizzBo;  //declare value 
+    if (n%5==0 && n%3==0){ //establish if/else statement of what possible outcomes could be based on the provided integer; in this line, it would be if the value is equally divisble by 5 and 3
       FizzBo = "FizzBuzz";
-    } else if (n%3 ==0 ){
+    } else if (n%3 ==0 ){ //this establishes an outcome of ONLY divisible by 3
       FizzBo = "Fizz";
-    } else if (n%5 == 0) {
+    } else if (n%5 == 0) { //this establishes an outcome of ONLY divisible by 5
       FizzBo = "Buzz";
-    } else {
+    } else {            //this establishes all other outcomes with the integer
       FizzBo = n.toString();
     }
-    console.log(FizzBo)
+    console.log(FizzBo) //this tests the solution works! 
     return FizzBo;
   }
   
