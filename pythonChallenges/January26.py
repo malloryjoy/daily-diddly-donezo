3ee5c41477161d05647a187ad9ec535166207527
Mallory Joy <malloryjoy13@gmail.com>
2026-01-26 19:26:50 -0500

    Added comments explaining solution

--- pythonChallenges/January26.py
+++ pythonChallenges/January26.py
@@ -5,18 +5,18 @@
 ##If the number is divisible by both 3 and 5, return "FizzBuzz".
 ##Otherwise, return the given number as a string.
 
-def fizz_buzz_mini(n):
-    fizzbo = ""
-    if n%5==0 and n%3==0:
-    	fizzbo = "FizzBuzz"
-    elif n%5==0:
+def fizz_buzz_mini(n): ##declares formula to get answer 
+    fizzbo = "" ##declares fizzbo value
+    if n%5==0 and n%3==0: ##declares fizzbo as FizzBuzz if n is divisible by 5 and 3
+    	fizzbo = "FizzBuzz" 
+    elif n%5==0:    ##declares fizzbo as Buzz if n is divisible by 5 
         fizzbo = "Buzz"
-    elif n%3==0:
+    elif n%3==0: ##declares fizzbo as Fizz if n is divisible by 3
         fizzbo = "Fizz"
-    else:
+    else: ##declares fizzbo as the provided n value as a string
         fizzbo = str(n)
-    print(fizzbo)
-    return fizzbo
+    print(fizzbo) ##checks to confirm answer populates correctly
+    return fizzbo ##returns solution 
 
 
 
