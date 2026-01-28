33adae2e9ce821796142223bfb0590fbd8dedfe1
Mallory Joy <malloryjoy13@gmail.com>
2026-01-27 19:54:39 -0500

    Added comment to explain process of conclusion

--- pythonChallenges/January27.py
+++ pythonChallenges/January27.py
@@ -4,20 +4,20 @@
 ##For example, given 1769472000000, a timestamp for January 27th, 2026, return "odd" because the day number (27) 
 ##is an odd number.
 
-from datetime import datetime, timezone
+from datetime import datetime, timezone ##imports date and timezone
 
-def odd_or_even_day(timestamp):
-    dt_object_utc = datetime.fromtimestamp(timestamp/1000, timezone.utc)
-    ##print(dt_object_utc)
-    day = dt_object_utc.day
-    ##print(day)
-    dateDivisible = "even"
-    if day%2 != 0:
+def odd_or_even_day(timestamp): ## takes timestamp as a parameter 
+    dt_object_utc = datetime.fromtimestamp(timestamp/1000, timezone.utc) ##creates object for converted timestamp, accounting for utc
+    print(dt_object_utc) ##checks that timestamp conversion is effective
+    day = dt_object_utc.day ##isolates the day of the previously created object
+    print(day) ##checks that day isolation worked 
+    dateDivisible = "even" ##establishes that the default of dateDivisible is "even"
+    if day%2 != 0: ## creates if/else statement that determines the setting of dateDivisible
         dateDivisible = "odd"
     else:
         dateDivisble = "even"
-    print(dateDivisible)
-    return dateDivisible
+    print(dateDivisible) ##checks that dateDivisble returns the correct answer 
+    return dateDivisible ##returns the correct answer! 
 
 
 odd_or_even_day(1769472000000) ##should return "odd"
