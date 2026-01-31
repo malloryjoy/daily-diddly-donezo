daa720973ac5ad853137d754bb91cb6d7163cea6
Mallory Joy <malloryjoy13@gmail.com>
2026-01-31 13:36:34 -0500

    Adds comments explaining thought process

--- pythonChallenges/January31.py
+++ pythonChallenges/January31.py
@@ -17,39 +17,50 @@
 
 
 
-def get_sign(date_str):
-    date_List = date_str.split("-")
-    ##print(date_List);
-    date = int(date_List[2])
-    month = date_List[1]
-    ##print(date)
-    ##print(month)
-    star_sign = ""
-    if (month == "01" and date >= 20) or (month == "02" and date <= 18): 
-      star_sign = "Aquarius";
-    elif (month == "02" and date >= 19) or (month == "03" and date <= 20):
-      star_sign = "Pisces";
+def get_sign(date_str): ##Create function that accepts date string as a parameter 
+    date_List = date_str.split("-") ##converts date string to a list, split by "-"
+    ##print(date_List), establishes correct answer
+    date = int(date_List[2]) ## assigns the date value as a number
+    month = date_List[1] ##assigns the month value as an item 
+    ##print(date), confirms the correct answer
+    ##print(month), confirms the correct answer 
+    star_sign = "" ##creates empty item for starsign
+    if (month == "01" and date >= 20) or (month == "02" and date <= 18): ##establishes if/else statement for each possible outcome 
+      star_sign = "Aquarius"
+    elif (month == "02" and date >= 19) or (month == "03" and date <= 20): 
+      star_sign = "Pisces"
     elif (month == "03" and date >= 21) or (month == "04" and date <= 19):
-      star_sign = "Aries";
+      star_sign = "Aries"
     elif (month == "04" and date >= 20) or (month == "05" and date <= 20):
-      star_sign = "Taurus";
+      star_sign = "Taurus"
     elif (month == "05" and date >= 21) or (month == "06" and date <= 20):
-      star_sign = "Gemini";
+      star_sign = "Gemini"
     elif (month == "06" and date >= 21) or (month == "07" and date <= 22):
-      star_sign = "Cancer";
+      star_sign = "Cancer"
     elif (month == "07" and date >= 23) or (month == "08" and date <= 22):
       star_sign = "Leo"
     elif (month == "08" and date >= 23) or (month == "09" and date <= 22):
-      star_sign = "Virgo";
+      star_sign = "Virgo"
     elif (month == "09" and date >= 23) or (month == "10" and date <= 22):
-      star_sign = "Libra";
+      star_sign = "Libra"
     elif (month == "10" and date >= 23) or (month == "11" and date <= 21):
-      star_sign = "Scorpio";
+      star_sign = "Scorpio"
     elif (month == "11" and date >= 22) or (month == "12" and date <= 21):
-      star_sign = "Sagittarius";
+      star_sign = "Sagittarius"
     elif (month == "12" and date >= 22) or (month == "01" and date <= 19):
-      star_sign = "Capricorn";
+      star_sign = "Capricorn"
     
     
-    print(star_sign)
-    return (star_sign)
+    print(star_sign) ##confirms correct answer 
+    return (star_sign) ##returns correct answer
+
+
+get_sign("2026-01-31") #should return "Aquarius".
+get_sign("2001-06-10") #should return "Gemini".
+get_sign("1985-09-07") #should return "Virgo".
+get_sign("2023-03-19") #should return "Pisces".
+get_sign("2045-11-05") #should return "Scorpio".
+get_sign("1985-12-06") #should return "Sagittarius".
+get_sign("2025-12-30") #should return "Capricorn".
+get_sign("2018-10-08") #should return "Libra".
+get_sign("1958-05-04") #should return "Taurus"
