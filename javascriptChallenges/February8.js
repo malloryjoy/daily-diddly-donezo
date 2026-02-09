ef4d53cb8b011f6e3d07d12d0c8ffea34a11e893
Mallory Joy <malloryjoy13@gmail.com>
2026-02-08 22:46:16 -0500

    Commentary providing walkthrough process

--- javascriptChallenges/February8.js
+++ javascriptChallenges/February8.js
@@ -4,18 +4,18 @@ Given an array of integers, where each value represents the number of targets hi
 Each round consists of 5 targets.
 Each missed target results in a 150 meter penalty loop. */
 
-function calculatePenaltyDistance(rounds) {
-    rounds.forEach(calcPen) 
+function calculatePenaltyDistance(rounds) { //Created function with rounds 
+    rounds.forEach(calcPen) //establishes forEach loop for each item in rounds array in which the calcPen function is applied 
     
-    function calcPen(item, index, arr) {
-      arr[index] = (5 - item) * 150 
-      //console.log(arr[index])
+    function calcPen(item, index, arr) { //takes the integer, its place in the array, and the array 
+      arr[index] = (5 - item) * 150  //applies the provided math: subtract the number of targets hit from 5, and multiply that number by 150 to establish the total penalty distance
+      //console.log(arr[index]), to confirm right numbers are calculated 
       
     } 
-    //console.log(rounds)
-    const penalty = rounds.reduce((acc, curr) => acc + curr, 0);
-  console.log(penalty);
-  return penalty;
+    //console.log(rounds), to confirm rounds array shows the right values 
+    const penalty = rounds.reduce((acc, curr) => acc + curr, 0); //creates final penalty value in which the values in the rounds array are added together 
+  console.log(penalty); //confirms that the returned value is correct 
+  return penalty; //returns correct value 
 }
 
 
