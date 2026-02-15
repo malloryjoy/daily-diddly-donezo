c829bdaab8acd88548ff30a22606dcdb212e9cfc
Mallory Joy <malloryjoy13@gmail.com>
2026-02-15 12:09:42 -0500

    Added commentary/feedback on solution

--- pythonChallenges/February15.py
+++ pythonChallenges/February15.py
@@ -13,7 +13,11 @@
 ## "Blue" if the adjusted steepness is greater than 0.1 and less than or equal to 0.25
 ## "Black" if the adjusted steepness is greater than 0.25
 
-
+## This one was annoying. I was able to get the Javascript version naturally, yay! This one should have been just as intuitive. Unfortunately 
+## I kept getting the same error and I couldn't figure out why. My logic was working great! What gives? I forgot to convert the 
+## hill_type variable correctly to meet Python's naming conventions. These are the clumsy mistakes that break a problem! I'm glad
+## I'm working on this now so I learn and do better later. Switiching the naming convention to the correct format made everything 
+## run perfectly. Very silly. 
 
 
 def get_hill_rating(drop, distance, hill_type):
