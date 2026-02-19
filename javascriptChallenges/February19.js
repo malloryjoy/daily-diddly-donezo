a1a3b7f678ae64e56054c1d98548ce8e3b5d76d8
Mallory Joy <malloryjoy13@gmail.com>
2026-02-19 16:30:04 -0500

    Provided setup

--- javascriptChallenges/February19.js
+++ javascriptChallenges/February19.js
@@ -1,3 +1,18 @@
+
+// Given the snow depth and slope of a mountain, determine if there's an avalanche risk.
+
+// The snow depth values are "Shallow", "Moderate", or "Deep".
+// Slope values are "Gentle", "Steep", or "Very Steep".
+// Return "Safe" or "Risky" based on this table:
+
+// "Shallow"	"Moderate"	"Deep"
+// "Gentle"	"Safe"	"Safe"	"Safe"
+// "Steep"	"Safe"	"Risky"	"Risky"
+// "Very Steep"	"Safe"	"Risky"	"Risky"
+
+
+
+
 function avalancheRisk(snowDepth, slope) {
     let avRisk = "";
     if (slope == "Gentle" || snowDepth == "Shallow" ){
@@ -9,4 +24,14 @@ function avalancheRisk(snowDepth, slope) {
     console.log(avRisk)
     return avRisk;
   }
+  
+  avalancheRisk("Shallow", "Gentle") //should return "Safe".
+  avalancheRisk("Shallow", "Steep") //should return "Safe".
+  avalancheRisk("Shallow", "Very Steep") //should return "Safe".
+  avalancheRisk("Moderate", "Gentle") //should return "Safe".
+  avalancheRisk("Moderate", "Steep") //should return "Risky".
+  avalancheRisk("Moderate", "Very Steep") //should return "Risky".
+  avalancheRisk("Deep", "Gentle") //should return "Safe".
+  avalancheRisk("Deep", "Steep") //should return "Risky".
+  avalancheRisk("Deep", "Very Steep") //should return "Risky"
   
--- pythonChallenges/February19.py
+++ pythonChallenges/February19.py
@@ -1,3 +1,16 @@
+#Given the snow depth and slope of a mountain, determine if there's an avalanche risk.
+
+#The snow depth values are "Shallow", "Moderate", or "Deep".
+#Slope values are "Gentle", "Steep", or "Very Steep".
+#Return "Safe" or "Risky" based on this table:
+
+#"Shallow"	"Moderate"	"Deep"
+#"Gentle"	"Safe"	"Safe"	"Safe"
+#"Steep"	"Safe"	"Risky"	"Risky"
+#"Very Steep"	"Safe"	"Risky"	"Risky"
+
+
+
 def avalanche_risk(snow_depth, slope):
     avRisk ="";
     if slope =="Gentle" or snow_depth == "Shallow":
