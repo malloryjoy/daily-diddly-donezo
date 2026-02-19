88365032083a54ffb608ae7ec087a451ee5e40b3
Mallory Joy <malloryjoy13@gmail.com>
2026-02-18 20:48:36 -0500

    Added commentary explaining process

--- javascriptChallenges/February18.js
+++ javascriptChallenges/February18.js
@@ -1,32 +1,32 @@
 function checkEligibility(athleteWeights, sledWeight) {
-    let bobSledgible = "";
-    let minWeight = true;
-    let maxWeight = true;
-    let numAthletes = athleteWeights.length;
-    //console.log(numAthletes);
-    const athleteKG = athleteWeights.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    //console.log(athleteKG);
-    if (numAthletes == 1 && sledWeight >= 162 || numAthletes == 2 && sledWeight >= 170 ||numAthletes == 4 && sledWeight >= 210) {
+    let bobSledgible = ""; //creates element to hold determined elgibility
+    let minWeight = true; //variable that holds confirmed minimum weight achieved 
+    let maxWeight = true; //variable that holds confirmed maximum weight
+    let numAthletes = athleteWeights.length; //variable that holds the length of the athlete weight arrays-- how many athletes to be put in the bobsled
+    //console.log(numAthletes); Confirms array length is correct 
+    const athleteKG = athleteWeights.reduce((accumulator, currentValue) => accumulator + currentValue, 0); //variable that holds the sum of the weight of each athlete
+    //console.log(athleteKG); Confirms weight is correct 
+    if (numAthletes == 1 && sledWeight >= 162 || numAthletes == 2 && sledWeight >= 170 ||numAthletes == 4 && sledWeight >= 210) { //if statement that declares the minimum weight of the bobsled is met 
       minWeight = true;
     } else {
       minWeight = false;
     }
-    //console.log(minWeight)
-    let totalWeight = sledWeight + athleteKG;
-    //console.log(totalWeight)
-    if (numAthletes == 1 && totalWeight <= 247 ||numAthletes == 2 && totalWeight <= 390 || numAthletes == 4 && totalWeight <= 630){
+    //console.log(minWeight); confirms weight is or isn't met 
+    let totalWeight = sledWeight + athleteKG; //variable that holds the combined weight of the sled and the athletes 
+    //console.log(totalWeight); confirms the total weight is accurate 
+    if (numAthletes == 1 && totalWeight <= 247 ||numAthletes == 2 && totalWeight <= 390 || numAthletes == 4 && totalWeight <= 630){ //if/else statement confirming  that the max weight is "met"
       maxWeight = true;
     } else {
       maxWeight = false;
     }
-    //console.log(maxWeight)
-    if (minWeight == true && maxWeight == true){
-      bobSledgible = "Eligible";
+    //console.log(maxWeight); confirms the weight is "met"
+    if (minWeight == true && maxWeight == true){ //if statement that confirms that the minimum weight is met and the maximum weight is not exceeded, confirming eligibility 
+      bobSledgible = "Eligible"; 
     } else {
       bobSledgible = "Not Eligible"
     }
-    console.log(bobSledgible)
-    return bobSledgible;
+    console.log(bobSledgible) //confirms eligibility status
+    return bobSledgible; //returns eligibility status 
   }
   
   
--- pythonChallenges/February18.py
+++ pythonChallenges/February18.py
@@ -1,32 +1,33 @@
 def check_eligibility(athlete_weights, sled_weight):
-    bob_sledgible = "Eligible"
-    min_weight = True
-    max_weight = True
-    num_athletes = len(athlete_weights)
-  ##print(num_athletes)
-    athleteKG = sum(athlete_weights)
-  ##print(athleteKG);
-    if num_athletes == 1 and sled_weight >= 162 or num_athletes == 2 and sled_weight >= 170 or num_athletes == 4 and sled_weight >= 210:
+    bob_sledgible = "Eligible" ##creates element to hold determined elgibility
+    min_weight = True ##variable that holds confirmed minimum weight achieved 
+    max_weight = True ##variable that holds confirmed maximum weight
+    num_athletes = len(athlete_weights) ##variable that holds the length of the athlete weight arrays-- how many athletes to be put in the bobsled
+  ##print(num_athletes); confirms the number of athletes to place in the bobsled 
+    athleteKG = sum(athlete_weights) ##variable that holds the sum of the weight of each athlete
+ 
+  ##print(athleteKG); confirms the athlete weight total
+    if num_athletes == 1 and sled_weight >= 162 or num_athletes == 2 and sled_weight >= 170 or num_athletes == 4 and sled_weight >= 210: ##if statement that declares the minimum weight of the bobsled is met 
         min_weight = True
     else:
         min_weight = False
   
-  ##print(min_weight)
-    total_weight = sled_weight + athleteKG
-  ##print(total_weight)
-    if num_athletes == 1 and total_weight <= 247 or num_athletes == 2 and total_weight <= 390 or num_athletes == 4 and total_weight <= 630:
+  ##print(min_weight), confirms the correct weight status 
+    total_weight = sled_weight + athleteKG ##variable that holds the combined weight of the sled and the athletes 
+  ##print(total_weight), confirms total weight 
+    if num_athletes == 1 and total_weight <= 247 or num_athletes == 2 and total_weight <= 390 or num_athletes == 4 and total_weight <= 630: ##if/else statement confirming  that the max weight is "met"
         max_weight = True
     else:
         max_weight = False
 
-  ##print(max_weight)
-    if min_weight == True and max_weight == True:
+  ##print(max_weight), confirms max weight status 
+    if min_weight == True and max_weight == True: ##if statement that confirms that the minimum weight is met and the maximum weight is not exceeded, confirming eligibility 
         bobSledgible = "Eligible"
     else:
         bobSledgible = "Not Eligible"
 
-    print(bobSledgible)
-    return bobSledgible
+    print(bobSledgible) ## confirms eligibility status 
+    return bobSledgible ## returns eligibility status 
 
 
 
