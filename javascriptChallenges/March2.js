7da7d26c1d00f2ab7c5166098a86927308b00f44
Mallory Joy <malloryjoy13@gmail.com>
2026-03-02 13:16:33 -0500

    Updated with comments

--- javascriptChallenges/March2.js
+++ javascriptChallenges/March2.js
@@ -6,14 +6,15 @@
 // Ignore all non-letter characters.
 
 function sumLetters(str) {
-    const cleanedText = str.toLowerCase().replace(/[^a-z]/g, '');
-    const positions = [...cleanedText].map(char => {
-      return char.charCodeAt(0) - 96;
+    const cleanedText = str.toLowerCase().replace(/[^a-z]/g, ''); //creates new string without special characters or spaces 
+    const positions = [...cleanedText].map(char => { //creates an array, with the array items consisting of the letters in the new 
+    //string, converted to their ASCII numeric value 
+      return char.charCodeAt(0) - 96; //converts the ASCII value to the corresponding alphanumeric value (a=1, b=2, c=3, etc.)
     });
-    //console.log(positions);
-    let finalAnswer = positions.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    console.log(finalAnswer);
-    return finalAnswer;
+    //console.log(positions); confirms that the array has been created  with the correct corresponding alphanumeric conversions
+    let finalAnswer = positions.reduce((accumulator, currentValue) => accumulator + currentValue, 0); //sums up all values in the array
+    console.log(finalAnswer); //confirms the correct answer 
+    return finalAnswer; //returns the final answer 
   }
   
   
