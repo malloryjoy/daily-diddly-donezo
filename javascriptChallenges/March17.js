d7fa9641143d33b6c67946eb45a74a743b1b7df2
Mallory Joy <malloryjoy13@gmail.com>
2026-03-17 14:10:23 -0400

    Added setup and solution

---
+++ javascriptChallenges/March17.js
@@ -0,0 +1,49 @@
+// Given an integer representing the number of years a couple has been married, return their most recent anniversary milestone according to this chart:
+
+// Years Married	Milestone
+// 1	"Paper"
+// 5	"Wood"
+// 10	"Tin"
+// 25	"Silver"
+// 40	"Ruby"
+// 50	"Gold"
+// 60	"Diamond"
+// 70	"Platinum"
+// If they haven't reached the first milestone, return "Newlyweds".
+
+
+function getMilestone(years) {
+    let milestone = "";
+    if (years < 1){
+      milestone = "Newlyweds"
+    } else if (years >= 1 && years < 5){
+      milestone = "Paper"
+    } else if (years >= 5 && years < 9){
+      milestone = "Wood"
+    } else if (years >= 10 && years < 25){
+      milestone = "Tin"
+    } else if (years >=25 && years < 40){
+      milestone = "Silver"
+    } else if (years >= 40 && years < 50){
+      milestone = "Ruby"
+    } else if (years >=50 && years < 60){
+      milestone = "Gold"
+    } else if (years >=60 && years < 70){
+      milestone = "Diamond"
+    } else if (years >= 70){
+      milestone ="Platinum"
+    }
+    console.log(milestone)
+    return milestone;
+  }
+  
+  
+  getMilestone(0) //should return "Newlyweds".
+  getMilestone(1) //should return "Paper".
+  getMilestone(8) //should return "Wood".
+  getMilestone(10) //should return "Tin".
+  getMilestone(26) //should return "Silver".
+  getMilestone(45) //should return "Ruby".
+  getMilestone(50) //should return "Gold".
+  getMilestone(64) //should return "Diamond".
+  getMilestone(71) //should return "Platinum"
