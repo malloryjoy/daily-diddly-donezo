d8ced7379e793a7cf8324ad4b7e11b0120c449cb
Mallory Joy <malloryjoy13@gmail.com>
2026-04-07 19:38:13 -0400

    Provided setup and solution

--- pythonChallenges/April7.py
+++ pythonChallenges/April7.py
@@ -4,3 +4,31 @@
 # A palindrome is a string that is the same forward and backward.
 # If it's not a palindrome, return "none".
 
+import math
+
+def palindrome_locator(s):
+    reversed = s[::-1]
+    #print(reversed)
+    my_answer = "",
+    if reversed != s:
+        my_answer = "none"
+    else: 
+        length = len(s)
+        middle_index = math.floor(length / 2)
+        if length % 2 != 0:
+            my_answer = s[middle_index]
+        else:
+            my_answer = s[middle_index - 1] + s[middle_index]
+    print(my_answer)
+
+
+    return my_answer
+
+
+
+
+palindrome_locator("racecar") ##should return "e".
+palindrome_locator("level") ##should return "v".
+palindrome_locator("freecodecamp") ##should return "none".
+palindrome_locator("noon") ##should return "oo".
+palindrome_locator("11100111") ##should return "00".
