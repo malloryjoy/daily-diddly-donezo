f68fbabc9b5953728835a7b7f4f71b18fc01249d
Mallory Joy <malloryjoy13@gmail.com>
2026-04-10 10:10:24 -0400

    Provided commentary explaining answer

--- javascriptChallenges/April10.js
+++ javascriptChallenges/April10.js
@@ -3,16 +3,22 @@
 // A standard chessboard is 8x8, with columns labeled A through H (left to right) and rows labeled 1 through 8 (bottom to top). Rooks can move as many squares as they want in a horizontal or vertical direction. So if they are on the same row or column, they can attack each other.
 
 function rookAttack(rook1, rook2) {
-    let rook1Array = rook1.split("")
-    let rook2Array = rook2.split("")
-    let isChess = true
-    //console.log(rook1Array)
-    //console.log(rook2Array)
-    if (rook1Array[0] == rook2Array[0] || rook1Array[1] == rook2Array[1]){
+    let rook1Array = rook1.split("") // Splits rook1 into an array by character
+    let rook2Array = rook2.split("") // Splits rook1 into an array by character
+    let isChess = true // creates default boolean value for determining if the rooks can attack
+    //console.log(rook1Array), confirms array was created correctly
+    //console.log(rook2Array), confirms array was created correctly
+    if (rook1Array[0] == rook2Array[0] || rook1Array[1] == rook2Array[1]){ //if else statement comparing the first and second values in the respective arrays, if either one are similar, they can attack
       isChess = true;
     } else{
       isChess = false;
     }
-    console.log(isChess)
-    return isChess
+    console.log(isChess) //logs to confirm the correct answer 
+    return isChess // returns the correct answer 
   }
+
+
+rookAttack("A1", "A8") //should return true.
+rookAttack("B4", "F4") //should return true.
+rookAttack("E3", "D4") //should return false.
+rookAttack("H7", "F6") //should return false.
