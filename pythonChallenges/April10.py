b774c24226eb43499faef618a5b20ac2c100f8e5
Mallory Joy <malloryjoy13@gmail.com>
2026-04-10 10:19:47 -0400

    Provided commentary explaining answer

--- pythonChallenges/April10.py
+++ pythonChallenges/April10.py
@@ -4,18 +4,18 @@
 
 
 def rook_attack(rook1, rook2):
-    rook1_list = list(rook1) 
-    rook2_list = list(rook2)
-    print(rook1_list)
-    print(rook2_list)
-    is_chess = True
-    if rook1_list[0] == rook2_list[0] or rook1_list[1] == rook2_list[1]:
+    rook1_list = list(rook1) ## creates list dividing rook1 by character 
+    rook2_list = list(rook2) ## creates list dividing rook2 by character
+    print(rook1_list) ## logs to confirm list created correctly
+    print(rook2_list) ## logs to confirm list created correctly
+    is_chess = True ## creates default boolean 
+    if rook1_list[0] == rook2_list[0] or rook1_list[1] == rook2_list[1]: ## if else statement declaring that the rooks may attack if either the first or last values of both lists are a match, returning false otherwise
         is_chess = True
     else:
         is_chess = False
-    print(is_chess)
+    print(is_chess) ## logs to confirm correct answer 
 
-    return is_chess
+    return is_chess ## returns correct answer 
 
 
 
