fc62a86a3e8502817a0cf28769eff41bccc86b31
Mallory Joy <malloryjoy13@gmail.com>
2026-04-12 15:49:51 -0400

    Provided setup

---
+++ javascriptChallenges/April12.js
@@ -0,0 +1,13 @@
+// Given a 2D matrix, return a flat array with all of its values in clockwise order.
+
+// The returned array should have the top-left value first, move right along the top row, then down the right column, then left along the bottom row, then up the left column. Repeat inward for any remaining layers.
+
+// For example, given:
+
+// [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ]
+// Return [1, 2, 3, 6, 9, 8, 7, 4, 5].
+
