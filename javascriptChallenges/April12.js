0c2e043446ab29ab205f1976c7e82a3ab4dd2443
Mallory Joy <malloryjoy13@gmail.com>
2026-04-12 15:53:17 -0400

    Provided solution

--- javascriptChallenges/April12.js
+++ javascriptChallenges/April12.js
@@ -11,3 +11,51 @@
 // ]
 // Return [1, 2, 3, 6, 9, 8, 7, 4, 5].
 
+function spiralMatrix(matrix) {
+    let newArray = [];
+    
+    if (matrix.length === 0) return newArray;
+    
+    let top = 0;
+    let bottom = matrix.length - 1;
+    let left = 0;
+    let right = matrix[0].length - 1;
+    
+    while (top <= bottom && left <= right) {
+      // Move right along the top row
+      for (let i = left; i <= right; i++) {
+        newArray.push(matrix[top][i]);
+      }
+      top++;
+      
+      // Move down the right column
+      for (let i = top; i <= bottom; i++) {
+        newArray.push(matrix[i][right]);
+      }
+      right--;
+      
+      // Move left along the bottom row (if there's still a row)
+      if (top <= bottom) {
+        for (let i = right; i >= left; i--) {
+            newArray.push(matrix[bottom][i]);
+        }
+        bottom--;
+      }
+      
+      // Move up the left column (if there's still a column)
+      if (left <= right) {
+        for (let i = bottom; i >= top; i--) {
+            newArray.push(matrix[i][left]);
+        }
+        left++;
+      }
+    }
+    
+    return newArray;
+  }
+
+
+spiralMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) //should return [1, 2, 3, 6, 9, 8, 7, 4, 5].
+spiralMatrix([["a", "b", "c", "d"], ["l", "m", "n", "e"], ["k", "p", "o", "f"], ["j", "i", "h", "g"]]) //should return ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"].
+spiralMatrix([[true, false, false], [false, true, true], [false, true, false], [true, true, false]]) //should return [true, false, false, true, false, false, true, true, false, false, true, true].
+spiralMatrix([[25, 24, 23, 22, 21], [10, 9, 8, 7, 20], [11, 2, 1, 6, 19], [12, 3, 4, 5, 18], [13, 14, 15, 16, 17]]) //should return [25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
