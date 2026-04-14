9206efe4c79a2552fe251eb78e2787dd610aa58b
Mallory Joy <malloryjoy13@gmail.com>
2026-04-14 18:55:40 -0400

    Provided commentary explaining answer

--- pythonChallenges/April14.py
+++ pythonChallenges/April14.py
@@ -3,34 +3,34 @@
 # If two or more letters tie for the last in the alphabet, return the first one.
 # Ignore all non-letter characters.
 
-import re
+import re ## import regex capability
 
 def get_last_letter(s):
-    cleaned_str = "".join(char for char in s if char.isalpha()) 
-    ## print(cleaned_str)
-    the_letter = " "
-    upper_count = sum(1 for char in cleaned_str if char.isupper())
-    if upper_count > 1:
-        sorted = re.sub(r'[a-z]', '', cleaned_str)
-        con_letter_array = list(sorted)
-        ## print(con_letter_array)
-        con_letter = [ord(char.lower()) - 64 for char in con_letter_array]
-        ## print(con_letter)
-        large_num = max(con_letter)
-        ## print(large_num)
-        the_letter = chr(64 + large_num).upper()
-        print(the_letter)
-        return the_letter
+    cleaned_str = "".join(char for char in s if char.isalpha()) ## creates new string filtering out non letters
+    ## print(cleaned_str), logs to confirm new string was created correctly 
+    the_letter = " " ## creates blank variable to hold correct answer 
+    upper_count = sum(1 for char in cleaned_str if char.isupper()) ##creates variable to get the count of capital letters in the string 
+    if upper_count > 1: ##applies the following code if there are more than one capital letters in the string
+        sorted = re.sub(r'[a-z]', '', cleaned_str) ## creates a new string that removes all the lowercase letters from the previous string
+        con_letter_array = list(sorted) ## creates a list from the filtered 'sorted' string
+        ## print(con_letter_array), logs to confirm list was created correctly 
+        con_letter = [ord(char.lower()) - 64 for char in con_letter_array] ## converts each letter in the list to the corresponding alphanumeric number
+        ## print(con_letter), logs to confirm letter list was created correctly
+        large_num = max(con_letter) ## returns the largest number in the list
+        ## print(large_num), logs to confirm the largest number in the list was returned
+        the_letter = chr(64 + large_num).upper() ## creates a variable that converts the largest number to the corresponding letter, then capitalizes the letter
+        print(the_letter) ## logs to confirm the correct answer 
+        return the_letter ## returns the correct answer
     else:
-        con_letter_array = list(cleaned_str)
-        ## print(con_letter_array)
-        con_letter = [ord(char.lower()) - 64 for char in con_letter_array]
-        ## print(con_letter)
-        large_num = max(con_letter)
-        ## print(large_num)
-        the_letter = chr(64 + large_num)
-        print(the_letter)
-        return the_letter
+        con_letter_array = list(cleaned_str) ## creates a list from the cleaned string 
+        ## print(con_letter_array), logs to confirm list was created correctly
+        con_letter = [ord(char.lower()) - 64 for char in con_letter_array] ## converts each letter to the corresponding number 
+        ## print(con_letter), logs to confirm letter list was created correctly
+        large_num = max(con_letter) ## returns largest number in the list 
+        ## print(large_num), logs to confirm the largest number in the list was returned
+        the_letter = chr(64 + large_num) ## reconverts largest number in the list to the corresponding letter in the alphabet
+        print(the_letter) ## logs to confirm the correct answer 
+        return the_letter ## returns the correct answer 
 
 
 
