c0c81ef3c03324b7c169e1b49f30327f402a0775
Mallory Joy <malloryjoy13@gmail.com>
2026-04-14 18:23:56 -0400

    Provided commentary explaining answer

--- javascriptChallenges/April14.js
+++ javascriptChallenges/April14.js
@@ -4,25 +4,25 @@
 // Ignore all non-letter characters.
 
 function getLastLetter(str) {
-    const cleanedStr = str.replace(/[^a-zA-Z]/g, "")
-    //console.log(cleanedStr)
-    let theLetter = "";
-    if ((cleanedStr.match(/[A-Z]/g) || []).length > 1) {
-      let sorted = cleanedStr.replace(/\p{Ll}/gu, "")
-      //console.log(sorted)
-      let conLetter = [...sorted].map(c => c.charCodeAt(0) - 64)
-      //console.log(conLetter)
-      let largeNum = Math.max(...conLetter)
-      let theLetter = String.fromCharCode(64+largeNum)
-      console.log(theLetter)
-      return theLetter
+    const cleanedStr = str.replace(/[^a-zA-Z]/g, "") //creates a string that filters out non-letters
+    //console.log(cleanedStr), confirms string has only letters 
+    let theLetter = ""; //creates blank variable to hold correct answer 
+    if ((cleanedStr.match(/[A-Z]/g) || []).length > 1) { //if else statement depending on if the string has more than two capital letters
+      let sorted = cleanedStr.replace(/\p{Ll}/gu, "") //replaces lowercase letters so only the capital letters are available for comparison
+      //console.log(sorted), confirms string only has capital letters
+      let conLetter = [...sorted].map(c => c.charCodeAt(0) - 64) //creates array that converts each letter in the string to the corresponding numerical value 
+      //console.log(conLetter), confirms array was generated correctly 
+      let largeNum = Math.max(...conLetter) //creates variable that holds the largest number in the array
+      let theLetter = String.fromCharCode(64+largeNum) //converts that letter into the corresponding capital letter 
+      console.log(theLetter) //confirms the correct answer 
+      return theLetter //returns the correct answer 
     } else{
-      let conLetter = [...cleanedStr].map(c => c.charCodeAt(0)- 96)
-      //console.log(conLetter)
-      let largeNum = Math.max(...conLetter)
-      let theLetter = String.fromCharCode(96+largeNum)
-      console.log(theLetter)
-      return theLetter
+      let conLetter = [...cleanedStr].map(c => c.charCodeAt(0)- 96) //creates array that converts each letter in the string to the corresponding numerical value 
+      //console.log(conLetter), confirms array was generated correctly 
+      let largeNum = Math.max(...conLetter) //creates variable that holds the largest number in the array 
+      let theLetter = String.fromCharCode(96+largeNum) //converts that letter into the corresponding lowercase letter 
+      console.log(theLetter) //confirms the correct answer 
+      return theLetter //returns the correct answer 
      
     }
     
