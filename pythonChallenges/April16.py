51a52c74ffb0c15c9438679269eee9717e4d978d
Mallory Joy <malloryjoy13@gmail.com>
2026-04-16 18:43:24 -0400

    Added solution

--- pythonChallenges/April16.py
+++ pythonChallenges/April16.py
@@ -4,3 +4,20 @@
 # If  is even and in the inclusive range of  to , print Not Weird
 # If  is even and in the inclusive range of  to , print Weird
 # If  is even and greater than , print Not Weird
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 != 0 or (n % 2 == 0 and (n >= 6 and n <=20) ):
+        print("Weird")
+    elif n%2 == 0 or (n%2 == 0 and (n > 20)):
+        print("Not Weird")
